fcdb277a8cb8b9af6e402722495f18f8db40b73a
Ali-Muhammad Zakaryaev <alimuhammadzakar@gmail.com>
2024-09-06 16:54:49 +0300

    I have deleted unnecessary code from MainViewModel and MainView

--- AlexKombat/AlexKombat/ViewModels/MainViewModel.cs
+++ AlexKombat/AlexKombat/ViewModels/MainViewModel.cs
@@ -1,10 +1,6 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace AlexKombat.ViewModels
+﻿namespace AlexKombat.ViewModels
 {
     public partial class MainViewModel : ViewModelBase
     {
-        [ObservableProperty]
-        private string _greeting = "Welcome to Avalonia!";
     }
 }
