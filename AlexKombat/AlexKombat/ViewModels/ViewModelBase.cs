7923db2370b3fcbfe6ed02cc0ae61edcbe053925
Ali-Muhammad Zakaryaev <alimuhammadzakar@gmail.com>
2024-09-06 16:52:19 +0300

    Add project files.

---
+++ AlexKombat/AlexKombat/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AlexKombat.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ AlexKombat/AlexKombat/Views/MainView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AlexKombat.Views
+{
+    public partial class MainView : UserControl
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
